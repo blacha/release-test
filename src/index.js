e1b4df4343378b375ee891e58650b908a7b703ea
Blayne Chard <bchard@linz.govt.nz>
2020-07-09 13:54:33 +1200

    feat: add a made up accesskey and see what happens!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 async function main() {
-    console.log('Lets try code scanning')
+    const accessKeyId = 'ASIAZSP744WXQPGDDHRW';
+    const secretKey = 'OoRDqIASH2M/9yNw3kR5NKr6cyP7pbTfR/dutkMW'
+    console.log('Lets try code scanning', { accessKeyId, secretKey})
 }
 
 main().catch(console.error)
