b5ab6d960c5f6a29cf7af79fb38823c4f141e84b
Blayne Chard <bchard@linz.govt.nz>
2020-07-09 14:02:32 +1200

    build: lets break the build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ async function main() {
     const accessKeyId = 'ASIAZSP744WXQPGDDHRW';
     const secretKey = 'OoRDqIASH2M/9yNw3kR5NKr6cyP7pbTfR/dutkMW'
     console.log('Lets try code scanning', { accessKeyId, secretKey})
+
+    function extend(dest, src) {
+        for (p in src)
+            dest[p] = src[p];
+        return dest;
+    }
+
+    console.log(extend({}, {bar: 'foo'}) )
 }
 
 main().catch(console.error)
