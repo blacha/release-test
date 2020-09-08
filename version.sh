a40e8d0cbc49bad1f8e1a15bf46e390f1b5072fa
Blayne Chard <blayne@chard.com>
2020-09-09 11:53:33 +1200

    feat: monorepo

--- version.sh
+++ version.sh
@@ -2,7 +2,7 @@
 
 git checkout master
 git pull --rebase
-#npx lerna version --conventional-commits
-npm version patch
+npx lerna version --conventional-commits
+# npm version patch
 CURRENT_VERSION=$(cat package.json | jq .version -r)
 git checkout -b release/v${CURRENT_VERSION}
