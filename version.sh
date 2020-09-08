28bf483d56bccd0eab81912e2c19271925c73c80
Blayne Chard <blayne@chard.com>
2020-09-09 11:59:28 +1200

    fix: tmp

--- version.sh
+++ version.sh
@@ -3,5 +3,5 @@
 git checkout master
 git pull --rebase
 npx lerna version --conventional-commits  --no-git-tag-version
-CURRENT_VERSION=$(cat package.json | jq .version -r)
-git checkout -b release/v${CURRENT_VERSION}
+CURRENT_VERSION=$(cat lerna.json | jq .version -r)
+# git checkout -b release/v${CURRENT_VERSION}
