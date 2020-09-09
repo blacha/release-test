42f80e58a7100eb8b38be415f505adc9299fa805
Blayne Chard <blayne@chard.com>
2020-09-09 12:01:54 +1200

    wip: try things

--- version.sh
+++ version.sh
@@ -2,6 +2,7 @@
 
 git checkout master
 git pull --rebase
-npx lerna version --conventional-commits  --no-git-tag-version
+npx lerna version --conventional-commits  --no-push
 CURRENT_VERSION=$(cat lerna.json | jq .version -r)
 # git checkout -b release/v${CURRENT_VERSION}
+echo $CURRENT_VERSION
