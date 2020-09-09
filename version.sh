f27a674645deb51e512b3b25a57033bbaf4bdb7b
Blayne Chard <blayne@chard.com>
2020-09-09 12:02:25 +1200

    wip: delete old tag

--- version.sh
+++ version.sh
@@ -5,4 +5,5 @@ git pull --rebase
 npx lerna version --conventional-commits  --no-push
 CURRENT_VERSION=$(cat lerna.json | jq .version -r)
 # git checkout -b release/v${CURRENT_VERSION}
+git tag -d v${CURRENT_VERSION}
 echo $CURRENT_VERSION
