9e8b7d0ec39032a79e333759633bd45e550d31de
Blayne Chard <blayne@chard.com>
2020-09-10 12:19:48 +1200

    wip: version

--- version.sh
+++ version.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -e
 
 git checkout master
 git pull --rebase
+
+npx lerna changed -a
 npx lerna version --conventional-commits  --no-push
+
 CURRENT_VERSION=$(cat lerna.json | jq .version -r)
-# git checkout -b release/v${CURRENT_VERSION}
+git checkout -b release/v${CURRENT_VERSION}
 git tag -d v${CURRENT_VERSION}
+
 echo $CURRENT_VERSION
