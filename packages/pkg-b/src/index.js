3ad48ff234e15dcb4bf01629d849377c6519dec0
Blayne Chard <blayne@chard.com>
2021-09-07 11:01:28 +1200

    feat(pkg-b): single handler

--- packages/pkg-b/src/index.js
+++ packages/pkg-b/src/index.js
@@ -2,6 +2,6 @@ import * as pkgA from '@blacha/pkg-a';
 
 module.exports ={
     handler() {
-        return pkgA.handler('Chicken') + pkgA.handler('Fish');
+        return pkgA.handler('Chicken');
     }
 }
