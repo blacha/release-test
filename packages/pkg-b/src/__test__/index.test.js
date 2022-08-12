33baa8c35f1b8585e3311d9e6802428d9764651c
Blayne Chard <blayne@chard.com>
2022-08-13 10:56:44 +1200

    test: more tests

---
+++ packages/pkg-b/src/__test__/index.test.js
@@ -0,0 +1,9 @@
+import test from 'node:test';
+import assert from 'node:assert';
+import * as pkgB from '../index.js';
+
+test('pkg-b', () => {
+  test('should return something', () => {
+    assert.notEqual(pkgB.handler(), null);
+  });
+});
