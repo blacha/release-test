88e1dd2f90f52facabecfd6f76a5658c2d74352a
Blayne Chard <blayne@chard.com>
2022-08-13 11:00:18 +1200

    ci: recsrive install

--- packages/pkg-b/src/__test__/index.test.js
+++ packages/pkg-b/src/__test__/index.test.js
@@ -5,5 +5,6 @@ import * as pkgB from '../index.js';
 test('pkg-b', () => {
   test('should return something', () => {
     assert.notEqual(pkgB.handler(), null);
+    assert.equal(pkgB.handler(), 'Hello World, Chicken!');
   });
 });
