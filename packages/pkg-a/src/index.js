ffefb8995189b8dcf4cf614aeb56ffdbb04efd0a
Blayne Chard <blayne@chard.com>
2023-07-05 12:20:20 +1200

    feat: remove all typescript

--- packages/pkg-a/src/index.js
+++
@@ -1,7 +0,0 @@
-export function handler(name) {
-  return `Hello World, ${name}!`;
-}
-
-export function goodBye(name) {
-  return `Good bye ${name}.`;
-}
--- packages/pkg-b/src/index.js
+++
@@ -1,5 +0,0 @@
-import * as pkgA from '@blacha/pkg-a';
-
-export const handler = () => {
-  return pkgA.handler('Blue');
-};
